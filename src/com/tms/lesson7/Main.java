15670aad1e550e159a8a2b6d7b6e7cb5f743ea01
Влад <vlad28031997@gmail.com>
2022-01-10 23:55:31 +0300

    !

---
+++ src/com/tms/lesson7/Main.java
@@ -0,0 +1,14 @@
+package com.tms.lesson7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user1=new User("Ivan","men",15);
+        User user2=new User("Petr","men",50);
+        User user3=new User("Tanya","women",26);
+
+
+
+    }
+}
---
+++ src/com/tms/lesson7/User.java
@@ -0,0 +1,23 @@
+package com.tms.lesson7;
+
+public class User {
+    private String name;
+    private String sex;
+    private int age;
+
+    public User(String name, String sex, int age) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
