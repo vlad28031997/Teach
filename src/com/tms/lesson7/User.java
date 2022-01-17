0c19574afe85a1833b9d00d627605d3120ac6fad
Влад <vlad28031997@gmail.com>
2022-01-17 21:46:36 +0300

    !

--- src/com/tms/lesson7/User.java
+++ src/com/tms/lesson7/User.java
@@ -1,6 +1,6 @@
 package com.tms.lesson7;
 
-public class User {
+public class User implements Comparable {
     private String name;
     private String sex;
     private int age;
@@ -10,14 +10,30 @@ public class User {
         this.sex = sex;
         this.age = age;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
 
+        User user = (User) o;
+
+        if (age != user.age) return false;
+        if (!name.equals(user.name)) return false;
+        return sex.equals(user.sex);
+    }
     @Override
     public int hashCode() {
-        return super.hashCode();
+        int result = name.hashCode();
+        result = 31 * result + sex.hashCode();
+        result = 31 * result + age;
+        return result;
     }
-
     @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+    public int compareTo(Object o) {
+        User out=(User) o;
+        if (age!=out.age) {
+            return age- out.age;
+        }
+        else return name.charAt(0)-out.name.charAt(0);
     }
 }
